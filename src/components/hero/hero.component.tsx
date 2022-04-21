cb999f3bbee43c1c5a5c5001b2f9278665c09752
Ilya Zaitsev <godested@gmail.com>
2022-04-22 00:50:41 +0300

    remove logo from hero

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -4,7 +4,6 @@ import { RootClassName } from 'components/root-classname';
 import { useLazyRef } from 'hooks';
 import { WithOptionalClassNameProps } from 'types';
 import { ExpectedRef } from 'utils/ref';
-import Logo from 'assets/icons/logo.inline.svg';
 import { HeroCanvas } from './hero-canvas';
 import * as styles from './hero.module.scss';
 
